0bfdb5b12cda5e581da07581936a76bc959c043a
Thomasyong11 <akumyongt27@gmail.com>
2020-10-07 14:29:38 +0000

    Added a side Navigation

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,17 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import style from "./header.module.css";
-const Header = () => {
+import FontAwesome from "react-fontawesome";
+import SideNav from "./SideNav/sidenav";
+
+const Header = (props) => {
+  const navBars = () => {
+    return (
+      <div className={style.bars}>
+        <FontAwesome
+          name="bars"
+          onClick={props.onOpenNav}
+          style={{
+            color: "#dfdfdf",
+            padding: "10px",
+            cursor: "pointer",
+          }}
+        />
+      </div>
+    );
+  };
   const logo = () => {
     return (
-      <Link to="/">
+      <Link to="/" className={style.logo}>
         <img alt="nba logo" src="/images/nba_logo.png" />
       </Link>
     );
   };
   return (
     <header className={style.header}>
-      <div className={style.headerOpt}>{logo()}</div>
+      <SideNav {...props} />
+      <div className={style.headerOpt}>
+        {navBars()}
+        {logo()}
+      </div>
     </header>
   );
 };
