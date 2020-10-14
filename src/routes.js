0b35907349c419f5d542b6f10b4352d97058ec2a
Thomasyong11 <akumyongt27@gmail.com>
2020-10-14 16:26:10 +0000

    added a route to articles

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import Layout from "./components/Hoc/Layout/layout";
 import Home from "./components/Home/home";
-
+import NewsArticle from "./components/Articles/News/Post/index";
 class Routes extends Component {
   render() {
     return (
       <Layout>
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/articles/:id" exact component={NewsArticle} />
         </Switch>
       </Layout>
     );
