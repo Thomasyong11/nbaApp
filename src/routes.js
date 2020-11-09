3e7ab817ab1d04fc1530fea18e812f189d88279d
Thomasyong11 <akumyongt27@gmail.com>
2020-11-09 10:35:38 +0000

    update side nav

--- src/routes.js
+++ src/routes.js
@@ -7,20 +7,18 @@ import Videoarticle from "./components/Articles/Videos/video/index";
 import NewsMain from "./components/Articles/News/Main/index";
 import VideosMain from "./components/Articles/Videos/main/index";
 import SignIn from "./components/signin/Signin";
-class Routes extends Component {
-  render() {
-    return (
-      <Layout>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/news" exact component={NewsMain} />
-          <Route path="/articles/:id" exact component={NewsArticle} />
-          <Route path="/videos/:id" exact component={Videoarticle} />
-          <Route path="/video" exact component={VideosMain} />
-          <Route path="/sign-in" exact component={SignIn} />
-        </Switch>
-      </Layout>
-    );
-  }
-}
+const Routes = (props) => {
+  return (
+    <Layout user={props.user}>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/news" exact component={NewsMain} />
+        <Route path="/articles/:id" exact component={NewsArticle} />
+        <Route path="/videos/:id" exact component={Videoarticle} />
+        <Route path="/video" exact component={VideosMain} />
+        <Route path="/sign-in" exact component={SignIn} />
+      </Switch>
+    </Layout>
+  );
+};
 export default Routes;
